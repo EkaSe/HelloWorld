86199b797fd1568e3892ff64310a5f4a1d874ca2
EkaSe <ekasemyash@ya.ru>
2016-09-16 09:49:27 +0300

    converting Func to class methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,104 +2,40 @@ using System;
 
 namespace HelloWorld
 {
-	public class Point2D
+	public class Miscellaneous
 	{
-		public double X;
-		public double Y;
-	}
+		static public void HiWorld () {
+			Console.WriteLine ("Hello World!");
+			Console.ReadKey ();
+		}
 
-	public class Rectangle
-	{
-		public Point2D LeftBottom;
-		public Point2D RightTop;
-	}
-
-	public class Circle
-	{
-		public Point2D Center;
-		public double Radius;
-	}
-
-
-	class MainClass
-	{
-//___________________________________________________________________________
-		static Func <double,double> sugarMassRequired = (thermosVolume) => {
+		static public double SugarMassRequired (double thermosVolume) {
 			int piecesOfSugarForCup = 3; //Количество кубиков рафинада на кружку
 			double cupVolume = 0.3; //Объем кружки (л)
 			double pieceOfSugarMass = 5; //Масса кубика рафинада (г)
+			return (thermosVolume * piecesOfSugarForCup * pieceOfSugarMass / cupVolume);
+		}
 
-			return thermosVolume * piecesOfSugarForCup * pieceOfSugarMass / cupVolume;
-		};
+		static public bool EnoughSugar (double thermosVolume, double sugarMass) {
+			return (SugarMassRequired(thermosVolume) <= sugarMass);
+		}
 
-		static Func <double,double,bool> enoughSugar = (thermosVolume, sugarMass) => {
-			if (sugarMassRequired(thermosVolume) > sugarMass)
-				return false;
-			else
-				return true;
-		};
+		static public bool TestSugar (){
+				//Собираюсь на пикник, хочу взять с собой термос чая. 
+				//Cколько мне в него грамм сахара засыпать? 
+				//В кружку я обычно три кубика кладу, но сейчас под рукой нет рафинада, 
+				//а хочу, чтобы чай был такой же по сладости.
 
-//___________________________________________________________________________ 
+				double userThermosVolume = 1.5; //Объем термоса (л)
+				double userSugarMass = 55; //Масса имеющегося сахара (г)
+				return EnoughSugar (userThermosVolume, userSugarMass);
+		}
+	}
 
-		static Func <double,double,double,bool> between = (value, min, max) => {
-			if (min > max)
-				throw new Exception("min не может быть больше max");
-			return ((value >= min) && (value <= max));
-		};
-
-		static Func <double, double, Point2D> constructPoint = (XCoordinate, YCoordinate) => {
-			Point2D point = new Point2D ();
-			point.X = XCoordinate;
-			point.Y = YCoordinate;
-			return point;
-		};
-
-		static Func <Point2D, Point2D, Rectangle> constructRectangle = (vertex1, vertex2) => {
-			Rectangle target = new Rectangle ();
-			target.LeftBottom = new Point2D ();
-			target.RightTop = new Point2D ();
-			target.LeftBottom.X = Math.Min(vertex1.X, vertex2.X);
-			target.LeftBottom.Y = Math.Min(vertex1.Y, vertex2.Y);
-			target.RightTop.X = Math.Max(vertex1.X, vertex2.X);
-			target.RightTop.Y = Math.Max(vertex1.Y, vertex2.Y);
-			return target;
-		};
-
-		static Func <Point2D, double, Circle> constructCircle = (center, radius) => {
-			Circle target = new Circle ();
-			target.Center = center;
-			target.Radius = radius;
-		};
-
-		static Func <Rectangle, Point2D, bool> shotInRectangleTarget = 
-			(target,shot) => {
-			return (between(shot.X, target.LeftBottom.X, target.RightTop.X) && 
-				between(shot.Y, target.LeftBottom.Y, target.RightTop.Y));
-		};
-
-		static Func <Point2D, Point2D, double> distanceBetweenPoints = (point1, point2) => {
-			return Math.Sqrt (Math.Pow((point1.X - point2.X),2) + Math.Pow((point1.Y - point2.Y),2));
-		};
-
-		static Func <Circle, Point2D, bool> shotInRoundTarget = 
-			(target, shot) => {
-			return (distanceBetweenPoints(target.Center, shot) <= target.Radius);
-		};
-
-//___________________________________________________________________________ 
-
-		static Action HiWorld = () => {
-			Console.WriteLine ("Hello World!");
-			Console.ReadKey ();
-			int result = 0;
-		};
-
-		static Action QuadEq = () => {
+	public class Algebra 
+	{
+		static public short QuadEq (double a, double b, double c) {
 			//Дано: коэффициенты уравнения a*x^2+b*x+c=0
-			double a, b, c; 
-			a = 1;
-			b = 2;
-			c = 1;
 			//Найти: вещественные корни уравнения с заданными коэффициентами
 			short NumberOfRealRoots = 0;
 			double root1 = 0;
@@ -110,8 +46,8 @@ namespace HelloWorld
 				double d;
 				d = b * b - 4 * a * c;
 				if (d >= 0) { // Решение: определение формулы в зависимости от числа вещественных корней
-					root1 = (-b + Math.Sqrt (d)) / a / 2;//вычисление
-					root2 = (-b - Math.Sqrt (d)) / a / 2;//вычисление
+					root1 = (-b + Math.Sqrt (d)) / a / 2;
+					root2 = (-b - Math.Sqrt (d)) / a / 2;
 					if (d == 0)
 						NumberOfRealRoots = 1;
 					else
@@ -119,7 +55,7 @@ namespace HelloWorld
 				} 
 			} else {//Решение: определение формулы для линейного уравнения
 				if (b != 0) {
-					root1 = -c / b;//Вычисление
+					root1 = -c / b;
 					NumberOfRealRoots = 1;
 				}
 			}
@@ -132,70 +68,119 @@ namespace HelloWorld
 				result1 = root1;
 				result2 = root2;
 			}
-	
-		};
-//___________________________________________________________________________ 
+			return NumberOfRealRoots;
+		}
 
+		static public void TestQuadEq () {
+			double a, b, c; 
+			a = 1;
+			b = 2;
+			c = 1;
+			short result = QuadEq (a, b, c);
+		}
+	}
+
+	public class Point2D
+	{
+		public double X;
+		public double Y;
+		static public Point2D Construct (double XCoordinate, double YCoordinate) {
+			Point2D point = new Point2D ();
+			point.X = XCoordinate;
+			point.Y = YCoordinate;
+			return point;
+		}
+		static public double CalculateDistance (Point2D point1, Point2D point2) {
+			return Math.Sqrt (Math.Pow((point1.X - point2.X),2) + Math.Pow((point1.Y - point2.Y),2));
+		}
+	}
+	
+	public class Rectangle
+	{
+		public Point2D LeftBottom;
+		public Point2D RightTop;
+
+		static public Rectangle Construct (Point2D vertex1, Point2D vertex2){
+			Rectangle target = new Rectangle ();
+			target.LeftBottom = new Point2D ();
+			target.RightTop = new Point2D ();
+			target.LeftBottom.X = Math.Min(vertex1.X, vertex2.X);
+			target.LeftBottom.Y = Math.Min(vertex1.Y, vertex2.Y);
+			target.RightTop.X = Math.Max(vertex1.X, vertex2.X);
+			target.RightTop.Y = Math.Max(vertex1.Y, vertex2.Y);
+			return target;
+		}
+	}
+
+	public class Circle
+	{
+		public Point2D Center;
+		public double Radius;
+
+		static public Circle Construct (Point2D center, double radius) {
+			Circle target = new Circle ();
+			target.Center = center;
+			target.Radius = radius;
+			return target;
+		}
+	}
+
+	public class HitTest
+	{
+		static public bool Between (double value, double min, double max) {
+			if (min > max)
+				throw new Exception("min не может быть больше max");
+			return ((value >= min) && (value <= max));
+		}
+
+		static public bool ShotInRectangleTarget (Rectangle target, Point2D shot) {
+			return (Between(shot.X, target.LeftBottom.X, target.RightTop.X) && 
+				Between(shot.Y, target.LeftBottom.Y, target.RightTop.Y));
+		}
+
+		static public bool ShotInRoundTarget (Circle target, Point2D shot) {
+			return (Point2D.CalculateDistance(target.Center, shot) <= target.Radius);
+		}
+
+		static public void TestHitTests (){
+			//В двухмерной системе координат есть прямоугольная мишень, 
+			//расположение и размеры которой заданы координатами противоположных углов.
+			//И есть координаты места попадания выстрела. 
+			//Нужно определить, пришлось ли попадание на мишень или нет.
+
+			Point2D shotPoint = Point2D.Construct (-1,3);
+			Point2D targetVertex1 = Point2D.Construct (-3, 3);
+			Point2D targetVertex2 = Point2D.Construct (1, 5);
+			Rectangle rectangleTarget = Rectangle.Construct (targetVertex1, targetVertex2);
+			bool resultRectangleTarget = ShotInRectangleTarget (rectangleTarget,shotPoint);
+
+			//Круглая мишень
+
+			Circle roundTarget = Circle.Construct (Point2D.Construct (0, 2), 3);
+			bool resultRoundTarget = ShotInRoundTarget (roundTarget, shotPoint);
+
+			//T-мишень
+
+			Rectangle TTargetPart1 = rectangleTarget;
+			Rectangle TTargetPart2 = Rectangle.Construct (Point2D.Construct (0, 0), Point2D.Construct (-2, 3));
+			bool resultTTarget = ShotInRectangleTarget (TTargetPart1, shotPoint) ||
+				HitTest.ShotInRectangleTarget (TTargetPart2, shotPoint);
+
+			//Мишень с отверстием
+
+			Circle targetHole = Circle.Construct (Point2D.Construct (2, 1), 2);
+			bool resultTargetWithHole = ShotInRoundTarget (roundTarget, shotPoint) &&
+				!ShotInRoundTarget (targetHole, shotPoint);
+		}
+			
+	}
+
+	class MainClass
+	{
 		static void Main (string[] args)
 		{
-			/*{
-				//Собираюсь на пикник, хочу взять с собой термос чая. 
-				//Cколько мне в него грамм сахара засыпать? 
-				//В кружку я обычно три кубика кладу, но сейчас под рукой нет рафинада, 
-				//а хочу, чтобы чай был такой же по сладости.
-
-				double userThermosVolume = 1.5; //Объем термоса (л)
-				double userSugarMass = 55; //Масса имеющегося сахара (г)
-				bool result = enoughSugar (userThermosVolume, userSugarMass);
-			}*/
-
-			{
-				//В двухмерной системе координат есть прямоугольная мишень, 
-				//расположение и размеры которой заданы координатами противоположных углов.
-				//И есть координаты места попадания выстрела. 
-				//Нужно определить, пришлось ли попадание на мишень или нет.
-
-				Point2D shotPoint = constructPoint (-1, 3);
-				Point2D targetVertex1 = constructPoint (-3, 3);
-				Point2D targetVertex2 = constructPoint (1, 5);
-				Rectangle rectangleTarget = constructRectangle (targetVertex1, targetVertex2);
-				bool resultRectangleTarget = shotInRectangleTarget (rectangleTarget,shotPoint);
-
-				//Круглая мишень
-
-				Circle roundTarget = constructCircle (constructPoint (0, 2), 3);
-				bool resultRoundTarget = shotInRoundTarget (roundTarget, shotPoint);
-
-				//T-мишень
-
-				Rectangle TTargetPart1 = rectangleTarget;
-				Rectangle TTargetPart2 = constructRectangle (constructPoint (0, 0),constructPoint (-2, 3));
-				bool resultTTarget = shotInRectangleTarget (TTargetPart1, shotPoint) ||
-				                     shotInRectangleTarget (TTargetPart2, shotPoint);
-				
-				//Мишень с отверстием
-
-				Circle targetHole = constructCircle (constructPoint (2, 1), 2);
-				bool resultTargetWithHole = shotInRoundTarget (roundTarget, shotPoint) &&
-				                            !shotInRoundTarget (targetHole, shotPoint);
-				
-			}
-
-
-			/*int iAct = 1;
-			Action ComboAction = () => Console.WriteLine("Empty Action");
-			ComboAction = () => {
-				QuadEq ();
-				sugar ();
-				if (iAct == 1) {
-					iAct = 0;
-					ComboAction ();
-				} else {
-					HiWorld ();
-				}
-			};
-			ComboAction ();
-			ComboAction ();*/
+			HitTest.TestHitTests ();
+			Miscellaneous.TestSugar ();
 		}
 	}
 }
